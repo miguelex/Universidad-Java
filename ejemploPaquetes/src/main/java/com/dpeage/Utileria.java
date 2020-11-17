71375d88a4a00a64e5fa0b87b95309c14cc10538
migue <miguelexd@hotmail.com>
2020-11-17 10:21:29 +0100

    Paquetes en Java

---
+++ ejemploPaquetes/src/main/java/com/dpeage/Utileria.java
@@ -0,0 +1,9 @@
+package com.dpeage;
+
+public class Utileria {
+    
+    public static void imprimir (String s) {
+        System.out.println("Cadena: " + s);
+    }
+    
+}
---
+++ ejemploPaquetes/src/main/java/ejemploPaquetes.java
@@ -0,0 +1,7 @@
+import com.dpeage.*;
+
+public class ejemploPaquetes {
+    public static void main(String[] args) {
+        Utileria.imprimir("Hola");
+    }
+}
